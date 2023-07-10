38bcc5e7a3e16a116c64d95dedb6fb4887399232
ahnaf.ahmad <ahnaf123>
2023-07-10 10:44:51 +0600

    Update Script

--- src/test/java/testRunner/CreateAccountTestRunner.java
+++ src/test/java/testRunner/CreateAccountTestRunner.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 import pages.CreateAccountPage;
 import pages.SignupPage;
 import setup.Setup;
+import utils.Utils;
 
 public class CreateAccountTestRunner extends Setup {
 
@@ -14,9 +15,11 @@ public class CreateAccountTestRunner extends Setup {
     public void signUpSuccessfully() throws InterruptedException {
         driver.get("https://www.automationexercise.com/login");
         signupPage = new SignupPage(driver);
+        Utils utils = new Utils();
+        utils.randomData();
 
         String newUserName = "Admin";
-        String newUserEmail = "admin4@gmail.com";
+        String newUserEmail = utils.getFirstName() + "@gmail.com";
 
         signupPage.doLogin(newUserName, newUserEmail);
         Thread.sleep(3000);
@@ -27,17 +30,30 @@ public class CreateAccountTestRunner extends Setup {
     public void enteredAccountInformationSuccessfully() throws InterruptedException {
 
         createAccountPage = new CreateAccountPage(driver);
+        Utils utils = new Utils();
+        utils.randomData();
 
         String password = "admin123";
-        String firstName = "Alen";
-        String lastName = "Shopon";
-        String company  = "MISL";
-        String address1 = "32/4";
-        String address2 = "North Street";
-        String state = "goalcoast";
-        String city = "perth";
-        String zipCode = "49763";
-        String mobileNumber = "01786734583";
+        String firstName = utils.getFirstName();
+        String lastName = utils.getLastName();
+        String company = utils.getCompany();
+        String address1 = utils.getAddress1();
+        String address2 = utils.getAddress2();
+        String state = utils.getState();
+        String city = utils.getCity();
+        String zipCode = utils.getZipCode();
+        String mobileNumber = utils.getMobileNumber();
+
+
+//        String firstName = "Alen";
+//        String lastName = "Shopon";
+//        String company  = "MISL";
+//        String address1 = "32/4";
+//        String address2 = "North Street";
+//        String state = "goalcoast";
+//        String city = "perth";
+//        String zipCode = "49763";
+//        String mobileNumber = "01786734583";
 
 
         createAccountPage.createAccount(password,firstName,lastName,company,address1,address2,state, city, zipCode, mobileNumber);
--- src/test/java/utils/Utils.java
+++ src/test/java/utils/Utils.java
@@ -1,4 +1,38 @@
 package utils;
 
+import com.github.javafaker.Faker;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Utils {
+
+    private String firstName;
+    private String lastName;
+    private String company;
+    private String address1;
+    private String address2;
+    private String state;
+    private String city;
+    private String zipCode;
+    private String mobileNumber;
+
+
+    public void randomData(){
+
+        Faker faker = new Faker();
+        setFirstName(faker.name().firstName());
+        setLastName(faker.name().lastName());
+        setCompany(faker.company().name());
+        setAddress1(faker.address().fullAddress());
+        setAddress2(faker.address().secondaryAddress());
+        setState(faker.address().state());
+        setCity(faker.address().city());
+        setZipCode(faker.address().zipCode());
+        setMobileNumber(faker.phoneNumber().cellPhone());
+
+
+    }
+
 }
